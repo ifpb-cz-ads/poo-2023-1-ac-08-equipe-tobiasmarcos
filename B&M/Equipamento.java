dfc833bd18ec0fe43fc2d8460e4781acf2b9c98a
Marcos Paulo <103538721+KingZabitus@users.noreply.github.com>
2023-05-03 22:34:35 -0300

    Atividades 3 a 8 do Batista & Moraes

---
+++ B&M/Equipamento.java
@@ -0,0 +1,25 @@
+public class Equipamento {
+    private String modelo;
+    private double preco;
+
+    public Equipamento(String modelo, double preco) {
+        this.modelo = modelo;
+        this.preco = preco;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+}
